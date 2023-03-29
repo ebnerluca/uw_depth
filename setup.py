1989c4e1629965f14f189e9c4c2d2ca6a57c1767
Luca Ebner <ebnerluca@hotmail.com>
2023-03-29 14:01:57 +1100

    Adjusted depth_estimation_evaluation to setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name="depth_estimation",
+    name="depth_estimation_evaluation",
     version="0.1",
-    description="The depth estimation package",
+    description="The depth estimation evaluation package",
     url="",
     author="Luca Ebner",
     author_email="luca.ebner@sydney.edu.au",
     license="MIT",
-    packages=["depth_estimation", "depth_estimation"],
-    package_dir={"depth_estimation": "depth_estimation"},
+    packages=["depth_estimation_evaluation"],  # , "depth_estimation"],
+    package_dir={"depth_estimation_evaluation": "depth_estimation_evaluation"},
     zip_safe=True,
 )
