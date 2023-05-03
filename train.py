- Bugfix: GPU memory allocation would increase during validation due to the fact that model.eval()  still allowed gradient computation. torch_nograd() fixes this and reduces reserved memory. - hyperparameter refactoring: hyperparameters are now global and not passed between functions

train.py
@@ -3,13 +3,9 @@ from torch.optim import AdamW
 from torch.cuda import is_available
 from torch.utils.data import DataLoader
 from torchvision import transforms
-from torchvision.utils import make_grid
-
-
 from torch.utils.tensorboard import SummaryWriter
 
 import time
-import datetime
 import os
 
 from depth_estimation.model.model import UDFNet
@@ -26,41 +22,28 @@ from depth_estimation.utils.visualization import get_tensorboard_grids
 # hyper parameters
 BATCH_SIZE = 4
 LEARNING_RATE = 0.0001
+LEARNING_RATE_DECAY = 1.0
 EPOCHS = 100
 LOSS_FN = CombinedLoss()
-DEVICE = "cpu"  # if cuda available it will be used
+DEVICE = "cuda" if is_available() else "cpu"
+
+# sampling parameters
+N_PRIORS_MAX = 100
+N_PRIORS_MIN = 100
+MU = 0.0
+STD_DEV = 10.0
 
 
-def train_model(learning_rate, batch_size, model_str=None):
-    """Main loop for training a model. Specify model type by giving a string such as 'UDFNet'."""
-    if model_str is None:
-        model_str = "UDFNet"
-
-    # device
-    global DEVICE
-    DEVICE = "cuda" if is_available() else "cpu"
-    print(f"Using device: {DEVICE}")
-
-    if model_str == "UDFNet":
-        print("Training UDFNet!")
-        train_UDFNet(learning_rate, batch_size)
-
-
-def train_UDFNet(
-    learning_rate, batch_size, learning_rate_decay=1.0, n_priors=100, device="cpu"
-):
+def train_UDFNet():
     """Train loop to train a UDFNet model."""
 
-    torch.autograd.set_detect_anomaly(True)
     # print run infos
-    run_name = (
-        f"udfnet_np{n_priors}_lr{learning_rate}_bs{batch_size}_lrd{learning_rate_decay}"
-    )
+    run_name = f"udfnet_np{N_PRIORS_MIN}-{N_PRIORS_MAX}_lr{LEARNING_RATE}_bs{BATCH_SIZE}_lrd{LEARNING_RATE_DECAY}"
     print(
         f"Training run {run_name} with parameters:\n"
-        + f"    learning rate: {learning_rate}\n"
-        + f"    learning rate decay: {learning_rate_decay}\n"
-        + f"    batch size: {batch_size}\n"
+        + f"    learning rate: {LEARNING_RATE}\n"
+        + f"    learning rate decay: {LEARNING_RATE_DECAY}\n"
+        + f"    batch size: {BATCH_SIZE}\n"
         + f"    device: {DEVICE}"
     )
 
@@ -81,15 +64,24 @@ def train_UDFNet(
             "/media/auv/Seagate_2TB/datasets/r20221109_064451_lizard_d2_077_vickis_v1/i20221109_064451_cv/train.csv",
         ],
         shuffle=True,
-        input_transform=transforms.Compose([Uint8PILToTensor()]),
+        input_transform=transforms.Compose(
+            [
+                Uint8PILToTensor(),
+            ]
+        ),
         target_transform=transforms.Compose(
             [
                 FloatPILToTensor(
                     normalize=True,
-                    zero_add=1e-10,  # avoid log(0)
                 ),
             ]
         ),
+        # both_transform=transforms.Compose(
+        #     [
+        #         transforms.RandomHorizontalFlip(),
+        #         transforms.RandomVerticalFlip(),
+        #     ]
+        # ),
     )
     validation_dataset = TrainDataset(
         pairs_csv_files=[
@@ -105,38 +97,34 @@ def train_UDFNet(
             [
                 FloatPILToTensor(
                     normalize=True,
-                    zero_add=1e-10,  # avoid log(0)
                 ),
             ]
         ),
     )
 
     # dataloaders
-    train_dataloader = DataLoader(train_dataset, batch_size=batch_size)
-    validation_dataloader = DataLoader(validation_dataset, batch_size=batch_size)
+    train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE)
+    validation_dataloader = DataLoader(validation_dataset, batch_size=BATCH_SIZE)
 
     for epoch in range(EPOCHS):
 
         # decayed learning rate
-        lr = learning_rate * (learning_rate_decay**epoch)
+        lr = LEARNING_RATE * (LEARNING_RATE_DECAY**epoch)
 
         # epoch info
         print("------------------------")
-        print(f"Epoch {epoch}/{EPOCHS} (lr: {lr}, batch_size: {batch_size})")
+        print(f"Epoch {epoch}/{EPOCHS} (lr: {lr}, batch_size: {BATCH_SIZE})")
         print("------------------------")
-
-        # optimizer
-        optimizer = AdamW(model.parameters(), lr=lr)
 
         # train epoch
         start_time = time.time()
         training_loss = train_epoch(
             dataloader=train_dataloader,
             model=model,
-            n_priors=n_priors,
+            learning_rate=lr,
+            n_priors_min=N_PRIORS_MIN,
+            n_priors_max=N_PRIORS_MAX,
             loss_fn=LOSS_FN,
-            optimizer=optimizer,
-            device=DEVICE,
             epoch=epoch,
         )
         print(f"Epoch time: {time.time() - start_time}")
@@ -145,8 +133,9 @@ def train_UDFNet(
         validation_loss = validate(
             dataloader=validation_dataloader,
             model=model,
+            n_priors_min=100,
+            n_priors_max=100,
             loss_fn=LOSS_FN,
-            device=DEVICE,
             epoch=epoch,
         )
 
@@ -159,26 +148,47 @@ def train_UDFNet(
 
 
 def train_epoch(
-    dataloader, model, loss_fn, optimizer, n_priors=100, device="cpu", epoch=0
+    dataloader,
+    model,
+    loss_fn,
+    learning_rate,
+    n_priors_min=100,
+    n_priors_max=100,
+    epoch=0,
 ):
-    """Train a model for one epoch."""
+    """Train a model for one epoch.
+    - model: The model to train
+    - loss_fn: The training objective loss function
+    - optimizer: The training optimizer
+    - n_priors: The number of depth priors to sample
+    - n_priors_min: If set, the number of samples is uniformly sampled between n_priors_min and and n_priors_max
+    - device: torch device
+    - epoch: epoch id"""
 
     # set training mode
     model.train()
 
-    # size = len(dataloader.dataset)
+    # optimizer
+    optimizer = AdamW(model.parameters(), lr=learning_rate)
+
     n_batches = len(dataloader)
     training_loss = 0.0
     created_grid = False
-    for batch_id, sample in enumerate(dataloader):
+    for batch_id, data in enumerate(dataloader):
 
         # move to device
-        X = sample[0].to(device)
-        y = sample[1].to(device)
+        X = data[0].to(DEVICE)  # RGB image
+        y = data[1].to(DEVICE)  # depth image
+
+        # set n_priors
+        if n_priors_max > n_priors_min:
+            n_priors = torch.randint(n_priors_min, n_priors_max, (1,)).item()
+        else:
+            n_priors = n_priors_max
 
         # get sparse prior parametrization
         prior = get_depth_prior_parametrization(
-            y, n_samples=n_priors, mu=0.0, std=10.0, normalize=True, device=device
+            y, n_samples=n_priors, mu=0.0, std=10.0, normalize=True, device=DEVICE
         )
 
         # prediction
@@ -203,6 +213,10 @@ def train_epoch(
                     rgb_target_pred_grid,
                 ) = get_tensorboard_grids(X, y, prior, pred, nrow=BATCH_SIZE)
 
+                # # move grids to cpu so summary writer does not reserve memory on gpu
+                # target_parametrization_grid = target_parametrization_grid.detach().cpu()
+                # rgb_target_pred_grid = rgb_target_pred_grid.detach().cpu()
+
                 # write to tensorboard
                 summary_writer.add_image(
                     "train_target_parametrization", target_parametrization_grid, epoch
@@ -210,7 +224,9 @@ def train_epoch(
                 summary_writer.add_image(
                     "train_rgb_target_pred", rgb_target_pred_grid, epoch
                 )
-                created_grid = True  # do only one grid to avoid data clutter
+
+                # do only one grid to avoid data clutter
+                created_grid = True
 
         if batch_id % 40 == 0:
             print(
@@ -222,49 +238,74 @@ def train_epoch(
     return avg_batch_loss
 
 
-def validate(dataloader, model, loss_fn, n_priors=100, device="cpu", epoch=0):
+def validate(
+    dataloader,
+    model,
+    loss_fn,
+    n_priors_min=100,
+    n_priors_max=100,
+    epoch=0,
+):
     """Validate a model, typically done after each training epoch."""
 
     # set evaluation mode
     model.eval()
 
-    # size = len(dataloader.dataset)
-    n_batches = len(dataloader)
-    validation_loss = 0.0
-    created_grid = False
-    for batch_id, sample in enumerate(dataloader):
+    # no gradients needed during evaluation
+    with torch.no_grad():
 
-        # move to device
-        X = sample[0].to(device)
-        y = sample[1].to(device)
+        n_batches = len(dataloader)
+        validation_loss = 0.0
+        created_grid = False
+        for batch_id, data in enumerate(dataloader):
 
-        # get sparse prior parametrization
-        prior = get_depth_prior_parametrization(
-            y, n_samples=n_priors, mu=0.0, std=10.0, normalize=True, device=device
-        )
+            # move to device
+            X = data[0].to(DEVICE)  # RGB image
+            y = data[1].to(DEVICE)  # depth image
 
-        # prediction
-        pred = model(X, prior)
+            # set n_priors
+            if n_priors_max > n_priors_min:
+                n_priors = torch.randint(n_priors_min, n_priors_max, (1,)).item()
+            else:
+                n_priors = n_priors_max
 
-        # tensorboard summary grids for visual inspection
-        if not created_grid:
-            if pred.shape[0] == BATCH_SIZE:
+            # get sparse prior parametrization
+            prior = get_depth_prior_parametrization(
+                y, n_samples=n_priors, mu=MU, std=STD_DEV, normalize=True, device=DEVICE
+            )
 
-                # get tensorboard grids
-                (
-                    target_parametrization_grid,
-                    rgb_target_pred_grid,
-                ) = get_tensorboard_grids(X, y, prior, pred, nrow=BATCH_SIZE)
+            # prediction
+            pred = model(X, prior)
 
-                # write to tensorboard
-                summary_writer.add_image(
-                    "target_parametrization", target_parametrization_grid, epoch
-                )
-                summary_writer.add_image("rgb_target_pred", rgb_target_pred_grid, epoch)
-                created_grid = True  # do only one grid to avoid data clutter
+            # tensorboard summary grids for visual inspection
+            if not created_grid:
+                if pred.shape[0] == BATCH_SIZE:
 
-        # add loss
-        validation_loss += loss_fn(pred, y).item()
+                    # get tensorboard grids
+                    (
+                        target_parametrization_grid,
+                        rgb_target_pred_grid,
+                    ) = get_tensorboard_grids(X, y, prior, pred, nrow=BATCH_SIZE)
+
+                    # move grids to cpu so summary writer does not reserve memory on gpu
+                    # target_parametrization_grid = (
+                    #     target_parametrization_grid.detach().cpu()
+                    # )
+                    # rgb_target_pred_grid = rgb_target_pred_grid.detach().cpu()
+
+                    # write to tensorboard
+                    summary_writer.add_image(
+                        "target_parametrization", target_parametrization_grid, epoch
+                    )
+                    summary_writer.add_image(
+                        "rgb_target_pred", rgb_target_pred_grid, epoch
+                    )
+
+                    # do only one grid to avoid data clutter
+                    created_grid = True
+
+            # add loss
+            validation_loss += loss_fn(pred, y).item()
 
     avg_batch_loss = validation_loss / n_batches
     print(f"Average batch validation_loss: {avg_batch_loss}")
@@ -285,5 +326,28 @@ def save_model(model, epoch, run_name):
     torch.save(model.state_dict(), model_filename)
 
 
+# def print_cuda_info():
+#     print("---")
+#     print(
+#         f"torch.cuda.memory_allocated: {torch.cuda.memory_allocated(0) / 1024 / 1024 / 1024}GB"
+#     )
+#     print(
+#         f"torch.cuda.memory_reserved: {torch.cuda.memory_reserved(0) / 1024 / 1024 / 1024}GB"
+#     )
+#     print(
+#         f"torch.cuda.max_memory_reserved: {torch.cuda.max_memory_reserved(0) / 1024 / 1024 / 1024}GB"
+#     )
+#     print("---")
+
+
+# def cuda_empty_cache():
+#     before = torch.cuda.memory_reserved(0) / 1024 / 1024 / 1024
+#     gc.collect()
+#     torch.cuda.empty_cache()
+#     freed = before - torch.cuda.memory_reserved(0) / 1024 / 1024 / 1024
+#     print(f"Freed {freed} GB of GPU memory.")
+
+
 if __name__ == "__main__":
-    train_model(LEARNING_RATE, BATCH_SIZE)
+
+    train_UDFNet()