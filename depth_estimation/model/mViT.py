Model bugfix, change output activation of mViT to softmax instead of relu

depth_estimation/model/mViT.py
@@ -40,9 +40,8 @@ class mViT(nn.Module):
             nn.Linear(256, 256),
             nn.LeakyReLU(),
             nn.Linear(256, n_bins + 1),
-            # nn.Sigmoid(),  # make sure outputs are [0,1] and no dead neurons
-            # nn.ReLU6(),  # keep signals bounded
-            nn.ReLU(),
+            # nn.ReLU(),
+            nn.Softplus(),  # replace ReLU because it causes dead neurons
         )
         self.n_bins = n_bins
 
@@ -70,7 +69,7 @@ class mViT(nn.Module):
         # estimating max depth and normed bin_widths
         eps = 0.1
         mlp_out = self.mlp(bins_head) + eps
-        max_depth = mlp_out[:, 0].unsqueeze(1) * 50.0
+        max_depth = mlp_out[:, 0].unsqueeze(1)
         bin_widths_normed = mlp_out[:, 1:]
         bin_widths_normed = bin_widths_normed / bin_widths_normed.sum(
             dim=1, keepdim=True

depth_estimation/model/model.py
@@ -90,7 +90,7 @@ class UDFNet(nn.Module):
         if self.max_depth is None:
             bin_edges = bin_edges_normed * pred_max_depth
         else:
-            bin_edges = bin_edges * self.max_depth
+            bin_edges = bin_edges_normed * self.max_depth
 
         # bin centers
         bin_centers = 0.5 * (bin_edges[:, :-1] + bin_edges[:, 1:])